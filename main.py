78ea4dc7d725ee6d387c707c81fd155a96acf5f3
kzahirni <kzahirni@cs-cl-13.cmpt.sfu.ca>
2022-07-04 06:55:13 -0700

    tansfering some results

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ parser.add_argument('-encoder', dest="encoder_type", default="AvePool",
 parser.add_argument('-batchSize', dest="batchSize", default=200,
                     help="the size of each batch; the number of graphs is the mini batch")
 parser.add_argument('-UseGPU', dest="UseGPU", default=True, help="either use GPU or not if availabel")
-parser.add_argument('-model', dest="model", default="KernelAugmentedWithTotalNumberOfTriangles",
+parser.add_argument('-model', dest="model", default="kipf",
                     help="KernelAugmentedWithTotalNumberOfTriangles is the only option in this rep")
 parser.add_argument('-device', dest="device", default="cuda:0", help="Which device should be used")
 parser.add_argument('-task', dest="task", default="graphGeneration", help="only option in this rep is graphGeneration")
